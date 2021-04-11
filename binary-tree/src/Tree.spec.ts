dd6222ef24bd9ed9f2f61e4b2f249888c76c0483
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2021-04-11 21:22:02 +0200

    minor fixes

--- binary-tree/src/Tree.spec.ts
+++ binary-tree/src/Tree.spec.ts
@@ -30,7 +30,7 @@ describe("Testing the correctness of calculations based on data from the task", 
 
   it("returns correct averages when the root of the subtree is the same as the main tree", () => {
     const subtree = tree.calculateSubtreeAverages(0 + usedNodeIds);
-    console.log("1: " + usedNodeIds);
+
     expect(subtree.mean).toEqual(3.8);
     expect(subtree.median).toEqual(4);
     expect(subtree.sum).toEqual(38);
@@ -38,7 +38,7 @@ describe("Testing the correctness of calculations based on data from the task", 
 
   it("returns correct averages when the root of the subtree is in the middle of the main tree", () => {
     const subtree = tree.calculateSubtreeAverages(6 + usedNodeIds);
-    console.log("2: " + usedNodeIds);
+
     expect(subtree.mean).toEqual(3.75);
     expect(subtree.median).toEqual(3.5);
     expect(subtree.sum).toEqual(15);
@@ -46,7 +46,7 @@ describe("Testing the correctness of calculations based on data from the task", 
 
   it("returns correct averages when the root of the subtree is a leaf of the main tree", () => {
     const subtree = tree.calculateSubtreeAverages(5 + usedNodeIds);
-    console.log("3: " + usedNodeIds);
+
     expect(subtree.mean).toEqual(1);
     expect(subtree.median).toEqual(1);
     expect(subtree.sum).toEqual(1);
